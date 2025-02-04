915d4c1ae4ba6da40e48b70767f4bed84fc4c7e5
alex-itx <aleksandr.ityaksov@gmail.com>
2025-02-04 20:50:19 +0300

    lesson 8

--- .venv/sitewomen/women/views.py
+++ .venv/sitewomen/women/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse, HttpResponseNotFound, Http404
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpResponseRedirect, HttpResponsePermanentRedirect
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
 def index(request):
     return HttpResponse("Страница приложения women.")
@@ -14,7 +15,8 @@ def categories_by_slug(request, cat_slug):
 
 def archive(request, year):
     if year > 2023:
-        raise Http404()
+        url_redirect = reverse('cats', args=('sport',))
+        return HttpResponsePermanentRedirect(url_redirect)
     return HttpResponse(f"<h1>Архив по годам</h1><p >{year}</p>")
 
 
