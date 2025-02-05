fa04866776409b32a959af3454fae15091167ae2
alex-itx <aleksandr.ityaksov@gmail.com>
2025-02-05 18:17:01 +0300

    lesson 14

--- .venv/sitewomen/sitewomen/settings.py
+++ .venv/sitewomen/sitewomen/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wqgc0movzent4hhkzgeb6f5&eqp*0d%65vvs5oalb@ou)2j9&)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'sitewomen.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
