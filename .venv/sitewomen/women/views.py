3d05edbc08df6df03d0aa217662022ade7b623f1
alex-itx <aleksandr.ityaksov@gmail.com>
2025-02-05 04:48:12 +0300

    lesson 13

--- .venv/sitewomen/women/views.py
+++ .venv/sitewomen/women/views.py
@@ -4,7 +4,11 @@ from django.urls import reverse
 from django.template.loader import render_to_string
 from django.template.defaultfilters import slugify
 
-menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
+menu = [{'title': "О сайте", 'url_name': 'about'},
+        {'title': "Добавить статью", 'url_name': 'add_page'},
+        {'title': "Обратная связь", 'url_name': 'contact'},
+        {'title': "Войти", 'url_name': 'login'}
+]
 
 data_db = [
     {'id': 1, 'title': 'Анджелина Джоли', 'content': 'Биография Анджелины Джоли', 'is_published': True},
@@ -25,20 +29,20 @@ def index(request):
 def about(request):
     return render(request, 'women/about.html', context={'title' : 'О сайте'})
 
-def categories(request, cat_id):
-    return HttpResponse(f"<h1>Статьи по категориям</h1><p >id:{cat_id}</p>")
+def show_post(request, post_id):
+    return HttpResponse(f"Отображение статьи с id = {post_id}")
 
-def categories_by_slug(request, cat_slug):
-    if request.POST:
-        print(request.POST)
-    return HttpResponse(f"<h1>Статьи по категориям</h1><p >slug:{ cat_slug }</p>")
 
-def archive(request, year):
-    if year > 2023:
-        url_redirect = reverse('cats', args=('sport',))
-        return HttpResponsePermanentRedirect(url_redirect)
-    return HttpResponse(f"<h1>Архив по годам</h1><p >{year}</p>")
+def addpage(request):
+    return HttpResponse("Добавление статьи")
 
+
+def contact(request):
+    return HttpResponse("Обратная связь")
+
+
+def login(request):
+    return HttpResponse("Авторизация")
 
 def page_not_found(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
