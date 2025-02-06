9ddae38100f90bd3bfff381a537599f93d3a9185
alex-itx <aleksandr.ityaksov@gmail.com>
2025-02-07 02:06:56 +0300

    lesson 22

--- .venv/sitewomen/women/models.py
+++ .venv/sitewomen/women/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.urls import reverse
 
 class Women(models.Model):
     title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, db_index=True, unique=True)
     content = models.TextField(blank=True)
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
@@ -15,3 +17,6 @@ class Women(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('post', kwargs={'post_slug': self.slug})
