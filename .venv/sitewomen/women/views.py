97458ef4e89f76dfc8a6fe5e5973fd61591d0bd0
alex-itx <aleksandr.ityaksov@gmail.com>
2025-02-07 17:29:28 +0300

    lesson 23

--- .venv/sitewomen/women/views.py
+++ .venv/sitewomen/women/views.py
@@ -12,14 +12,6 @@ menu = [{'title': "О сайте", 'url_name': 'about'},
         {'title': "Войти", 'url_name': 'login'}
 ]
 
-data_db = [
-    {'id': 1, 'title': 'Анджелина Джоли', 'content': '''<h1>Анджелина Джоли</h1> (англ. Angelina Jolie[7], при рождении Войт (англ. Voight), ранее Джоли Питт (англ. Jolie Pitt); род. 4 июня 1975, Лос-Анджелес, Калифорния, США) — американская актриса кино, телевидения и озвучивания, кинорежиссёр, сценаристка, продюсер, фотомодель, посол доброй воли ООН.
-Обладательница премии «Оскар», трёх премий «Золотой глобус» (первая актриса в истории, три года подряд выигравшая премию) и двух «Премий Гильдии киноактёров США».''',
-     'is_published': True},
-    {'id': 2, 'title': 'Марго Робби', 'content': 'Биография Марго Робби', 'is_published': False},
-    {'id': 3, 'title': 'Джулия Робертс', 'content': 'Биография Джулия Робертс', 'is_published': True},
-]
-
 cats_db = [
     {'id': 1, 'name': 'Актрисы'},
     {'id': 2, 'name': 'Певицы'},
@@ -28,7 +20,7 @@ cats_db = [
 
 
 def index(request):
-    posts = Women.objects.filter(is_published=1)
+    posts = Women.published.all()
     data = {
         'title': 'Главная страница',
         'menu': menu,
@@ -65,13 +57,16 @@ def contact(request):
 def login(request):
     return HttpResponse("Авторизация")
 
+
 def show_category(request, cat_id):
     data = {
-        'title': 'Главная страница',
+        'title': 'Отображение по рубрикам',
         'menu': menu,
-        'posts': data_db,
+        'posts': Women.published.all(),
         'cat_selected': cat_id,
     }
+
+    return render(request, 'women/index.html', context=data)
 
     return render(request, 'women/index.html', context=data)
 
