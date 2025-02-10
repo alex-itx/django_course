1be017148ddb9fa9b1be166aad66ecd9a634852e
alex-itx <aleksandr.ityaksov@gmail.com>
2025-02-10 16:07:21 +0300

    lesson 39

--- .venv/sitewomen/women/admin.py
+++ .venv/sitewomen/women/admin.py
@@ -4,12 +4,27 @@ from .models import Women, Category
 
 @admin.register(Women)
 class WomenAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'time_create', 'is_published', 'cat')
+    list_display = ('id', 'title', 'time_create', 'is_published', 'cat', 'brief_info')
     list_display_links = ('id', 'title')
     ordering = ['-time_create', 'title']
     list_editable = ('is_published', 'cat')
     list_per_page = 5
 
+    actions = ['set_published', 'set_draft']
+
+    @admin.display(description="Краткое описание", ordering='content')
+    def brief_info(self, women: Women):
+        return f"Описание {len(women.content)} символов."
+
+    @admin.action(description="Опубликовать выбранные записи")
+    def set_published(self, request, queryset):
+        count = queryset.update(is_published=Women.Status.PUBLISHED)
+        self.message_user(request, f"Изменено {count} записи(ей).")
+
+    @admin.action(description="Снять с публикации выбранные записи")
+    def set_draft(self, request, queryset):
+        count = queryset.update(is_published=Women.Status.DRAFT)
+        self.message_user(request, f"{count} записи(ей) сняты с публикации!", messages.WARNING)
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
