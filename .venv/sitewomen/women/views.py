17e04630a014eb97ebc7ccd55d7022e0c3e134c0
alex-itx <aleksandr.ityaksov@gmail.com>
2025-02-11 14:36:53 +0300

    lesson 44

--- .venv/sitewomen/women/views.py
+++ .venv/sitewomen/women/views.py
@@ -4,6 +4,7 @@ from django.urls import reverse
 from django.template.loader import render_to_string
 from django.template.defaultfilters import slugify
 from .models import Women, Category, TagPost
+from .forms import AddPostForm
 
 menu = [{'title': "О сайте", 'url_name': 'about'},
         {'title': "Добавить статью", 'url_name': 'add_page'},
@@ -40,7 +41,18 @@ def show_post(request, post_slug):
 
 
 def addpage(request):
-    return render(request, 'women/addpage.html', {'menu': menu, 'title': 'Добавление статьи'})
+    if request.method == 'POST':
+        form = AddPostForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+    else:
+        form = AddPostForm()
+    data = {
+        'menu': menu,
+        'title': 'Добавление статьи',
+        'form': form
+    }
+    return render(request, 'women/addpage.html', data)
 
 
 def contact(request):
