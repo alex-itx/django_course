d3718a2b71a0d618b3a4022790956691c337ef2d
alex-itx <aleksandr.ityaksov@gmail.com>
2025-02-14 19:15:04 +0300

    lesson 59

--- .venv/sitewomen/sitewomen/settings.py
+++ .venv/sitewomen/sitewomen/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django_extensions',
     'women.apps.WomenConfig',
     "debug_toolbar",
+    "users.apps.UsersConfig",
 ]
 
 MIDDLEWARE = [
--- .venv/sitewomen/sitewomen/urls.py
+++ .venv/sitewomen/sitewomen/urls.py
@@ -28,6 +28,7 @@ from debug_toolbar.toolbar import debug_toolbar_urls
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('women.urls')),
+    path('users/', include('users.urls', namespace="users")),
     path("__debug__/", include("debug_toolbar.urls")),
 ]
 
