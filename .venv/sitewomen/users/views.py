b0f66b439c2eeea3b9c32dcadae9a15ccbe9557e
alex-itx <aleksandr.ityaksov@gmail.com>
2025-02-15 21:32:09 +0300

    lesson 64

--- .venv/sitewomen/users/views.py
+++ .venv/sitewomen/users/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.views import LoginView
-from users.forms import LoginUserForm
+from django.shortcuts import render
+
+from users.forms import LoginUserForm, RegisterUserForm
 
 
 class LoginUser(LoginView):
@@ -7,3 +9,15 @@ class LoginUser(LoginView):
     template_name = 'users/login.html'
     extra_context = {'title': "Авторизация"}
 
+
+def register(request):
+    if request.method == "POST":
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)  # создание объекта без сохранения в БД
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            return render(request, 'users/register_done.html')
+    else:
+        form = RegisterUserForm()
+    return render(request, 'users/register.html', {'form': form})
