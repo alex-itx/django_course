6d652194484127505255f4807258e84696eb9015
alex-itx <aleksandr.ityaksov@gmail.com>
2025-02-15 17:11:15 +0300

    lesson 62

--- .venv/sitewomen/sitewomen/settings.py
+++ .venv/sitewomen/sitewomen/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+from django.conf.global_settings import LOGIN_REDIRECT_URL, LOGOUT_REDIRECT_URL
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -140,5 +141,6 @@ MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+LOGIN_REDIRECT_URL = 'home'
 
-
+LOGOUT_REDIRECT_URL = 'home'
