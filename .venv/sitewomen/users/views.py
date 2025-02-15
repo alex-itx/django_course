3d4dc28ca464758f3f46fd008d90e3625773e118
alex-itx <aleksandr.ityaksov@gmail.com>
2025-02-15 23:29:08 +0300

    lesson 65

--- .venv/sitewomen/users/views.py
+++ .venv/sitewomen/users/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.views import LoginView
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
 from users.forms import LoginUserForm, RegisterUserForm
 
@@ -10,14 +12,8 @@ class LoginUser(LoginView):
     extra_context = {'title': "Авторизация"}
 
 
-def register(request):
-    if request.method == "POST":
-        form = RegisterUserForm(request.POST)
-        if form.is_valid():
-            user = form.save(commit=False)  # создание объекта без сохранения в БД
-            user.set_password(form.cleaned_data['password'])
-            user.save()
-            return render(request, 'users/register_done.html')
-    else:
-        form = RegisterUserForm()
-    return render(request, 'users/register.html', {'form': form})
+class RegisterUser(CreateView):
+    form_class = RegisterUserForm
+    template_name = 'users/register.html'
+    extra_context = {'title': "Регистрация"}
+    success_url = reverse_lazy('users:login')
