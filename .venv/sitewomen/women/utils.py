b9534c59d57ea34f0c4a4467ca1cb8211906568f
alex-itx <aleksandr.ityaksov@gmail.com>
2025-02-15 14:02:40 +0300

    lesson 61

--- .venv/sitewomen/women/utils.py
+++ .venv/sitewomen/women/utils.py
@@ -14,14 +14,11 @@ class DataMixin:
         if self.title_page:
             self.extra_context['title'] = self.title_page
 
-        if 'menu' not in self.extra_context:
-            self.extra_context['menu'] = menu
 
     def get_mixin_context(self, context, **kwargs):
         if self.title_page:
             context['title'] = self.title_page
 
-        context['menu'] = menu
         context['cat_selected'] = None
         context.update(kwargs)
         return context
