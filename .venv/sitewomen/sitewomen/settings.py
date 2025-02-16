fae2ce3ddea60a7602bac40a4926a720107cdb28
alex-itx <aleksandr.ityaksov@gmail.com>
2025-02-16 16:21:37 +0300

    lesson 70

--- .venv/sitewomen/sitewomen/settings.py
+++ .venv/sitewomen/sitewomen/settings.py
@@ -148,4 +148,13 @@ AUTHENTICATION_BACKENDS = [
     'users.authentication.EmailAuthBackend',
 ]
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.yandex.ru"
+EMAIL_PORT = 465
+EMAIL_HOST_USER = "aleksandr.ityaksov.dev@yandex.ru"
+EMAIL_HOST_PASSWORD = "mncotzcqmqapbruf"
+EMAIL_USE_SSL = True
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+SERVER_EMAIL = EMAIL_HOST_USER
+EMAIL_ADMIN = EMAIL_HOST_USER
+
